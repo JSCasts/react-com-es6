062accac75af9ed37dde40da7686822d9b1e4c44
matheus <matheusml90@gmail.com>
2016-06-26 20:45:16 -0300

    Adding const, arrow function and destructuring

--- app/components/UserInfo.js
+++ app/components/UserInfo.js
@@ -1,20 +1,20 @@
-var React = require('react');
+import React from 'react';
 
-var UserRepos = require('./UserRepos');
+import UserRepos from './UserRepos';
 
-function UserInfo(props) {
-  var userInfo = props.user ?
+const UserInfo = ({user, repos}) => {
+  const userInfo = user ?
     (
       <div className="row">
         <div className="col-lg-4">
-          <img className="img-circle" src={props.user.avatar_url} alt="avatar" width="140" height="140" />
-          <h2>{props.user.login}</h2>
-          <p>{props.user.name}</p>
-          <p>Followers: {props.user.followers} / Following: {props.user.following}</p>
-          <p><a className="btn btn-default" href={props.user.html_url} role="button">View details</a></p>
+          <img className="img-circle" src={user.avatar_url} alt="avatar" width="140" height="140" />
+          <h2>{user.login}</h2>
+          <p>{user.name}</p>
+          <p>Followers: {user.followers} / Following: {user.following}</p>
+          <p><a className="btn btn-default" href={user.html_url} role="button">View details</a></p>
         </div>
         <div className="col-lg-8">
-          <UserRepos repos={props.repos} />
+          <UserRepos repos={repos} />
         </div>
       </div>
     ) : null;
@@ -27,4 +27,4 @@ UserInfo.propTypes = {
   repos: React.PropTypes.array,
 };
 
-module.exports = UserInfo;
+export default UserInfo;
